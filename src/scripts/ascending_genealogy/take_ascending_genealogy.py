22ab812dde75abb975a2435d01f1eb947db0be4c
Andrii Serdiuk <andrii.serdiuk@ukma.edu.ua>
2025-07-23 22:39:33 +0200

    Minor adjustments

--- src/scripts/ascending_genealogy/take_ascending_genealogy.py
+++ src/scripts/ascending_genealogy/take_ascending_genealogy.py
@@ -1,12 +1,21 @@
 from lineagekit.core.GenGraph import GenGraph
+from lineagekit.utility.utility import get_file_path, get_non_existing_path
 
-# Ask the user to specify the path to the pedigree
-genealogy_path = input("Specify the path to the genealogy file: ")
-# Parse the file
-genealogy_graph = GenGraph.get_graph_from_file(filepath=genealogy_path)
-# Specify the list of probands
-probands = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
-# Calculate the vertices in the ascending graph. This operation does not modify the graph
-vertices_in_ascending_graph = genealogy_graph.get_ascending_vertices_from_probands(probands=probands)
-# Remove all the vertices that are not in the ascending graph
-genealogy_graph.reduce_to_ascending_graph(probands=probands)
+
+def run_interactive_session():
+    # Ask the user to specify the path to the pedigree
+    genealogy_path = get_file_path("Specify the path to the genealogy file: ")
+    result_filepath = get_non_existing_path("Specify the path to the result file: ")
+    # Parse the file
+    genealogy_graph = GenGraph.get_graph_from_file(filepath=genealogy_path, missing_parent_notation=['0'])
+    # Specify the list of probands
+    probands = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+    # Calculate the vertices in the ascending graph. This operation does not modify the graph
+    vertices_in_ascending_graph = genealogy_graph.get_ascending_vertices_from_probands(probands=probands)
+    # Remove all the vertices that are not in the ascending graph
+    genealogy_graph.reduce_to_ascending_graph(probands=probands)
+    genealogy_graph.save_to_file(filepath=result_filepath, missing_parent_notation='0')
+
+
+if __name__ == '__main__':
+    run_interactive_session()
